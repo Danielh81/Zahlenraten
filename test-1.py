c85610f4f18a6cb671a38a0a9721cb207c143ecc
Danielh81 <116546040+Danielh81@users.noreply.github.com>
2022-10-28 15:13:30 +0200

    new try

--- test-1.py
+++ test-1.py
@@ -1,5 +1,3 @@
-
-from curses.ascii import isdigit
 import random
 
 min = 1
@@ -10,7 +8,7 @@ name = input("Wie lautet ihr Name: ")
 while len(name) == 0:
     name = input("Bitte geben Sie erneut Ihren Namen ein: ")
    
-print(f'Hallo, {name}! Ich denke an eine Zahl zwischen 1 und 100. Welche Zahl ist es?.')
+print(f'Hallo, {name}! Ich denke an eine Zahl zwischen {min} und {max}. Welche Zahl ist es?.')
 
 count = 0
 running = True
@@ -26,44 +24,46 @@ while running:
                 print('Das ist keine Zahl')
         
         guess = int(guess)
-        x = abs(number-guess)
-        if x <= 5 and guess != number: 
-            print('heiß')
-           
-        elif x >= 6 and x <= 10:
-            print ('schon wärmer')    
         
-        elif x >= 11:
-            print ('leider kalt')    
+        if guess < min:
+            print(f'Deine Zahl ist zu klein! ({min}-{max})')   
+            continue     
+        if guess > max:  
+            print(f'Deine Zahl ist zu groß! ({min}-{max})')  
+            continue
 
+        difference = abs(number-guess)
+        
+        if difference <= 5 and guess != number: 
+            print('heiß')        
+        elif difference >= 6 and difference <= 10:
+            print ('schon wärmer')           
+        elif difference >= 11:
+            print ('leider kalt')    
         if guess < number:
             print('Deine Versuch ist zu niedrig. Rate erneut!')
             count += 1
-            print(f'Deine Versuche: {count}')
-            
+            print(f'Deine Versuche: {count}')            
         if guess > number:
             print('Dein Versuch ist zu hoch. Rate erneut!')
             count += 1
             print(f'Deine Versuche: {count}')
-
         if guess == number:
             print(f'Gut gemacht! Du hast die richtige Zahl erraten!')
             count += 1
             print(f'Deine Versuche: {count}')
             break
 
-        if guess < min:
-            print(f'Deine Zahl ist zu klein! ({min}-{max})')        
-        if guess > max:  
-            print(f'Deine Zahl ist zu groß! ({min}-{max})')  
-
-
-    nochmal = input('Noch einmal? (j/n)')
-    if nochmal == 'j':
-        print("Here we go again!")
-        number = random.randint(min, max)
-        count = 0
-    
-    if nochmal == 'n':
-        running = False
-        print("Schade, vielleicht an anderes mal.")
+    while True:
+        nochmal = input('Noch einmal? (j/n)')
+        if nochmal == 'j':
+            print("Here we go again!")
+            number = random.randint(min, max)
+            count = 0      
+            break   
+        elif nochmal == 'n':
+            running = False
+            print("Schade, vielleicht an anderes mal.")
+            break         
+        else:
+            print('Bitte nur mit j/n antworten')    
